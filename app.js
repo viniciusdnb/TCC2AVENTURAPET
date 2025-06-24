40dbf1b783fe6032f88a3152cbf92c4b3116731f
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-06-24 14:17:24 -0300

    fix struture app

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express;
+const app = express();
 const port = 3000;
 
 app.get('/', function(req, res){
-    res.send('hello world');
+    res.send('hello word!');
 });
 
 app.listen(port, function(){
-    console.log('app online')
+    console.log(`app online in ${port}`);
 });
